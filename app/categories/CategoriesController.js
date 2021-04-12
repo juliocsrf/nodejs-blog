000119d93bddb673538ab3729ad7f06ff281b462
Julio Cesar <juliocsrmf@gmail.com>
2021-04-12 13:37:42 -0300

    Edição de categoria

--- app/categories/CategoriesController.js
+++ app/categories/CategoriesController.js
@@ -9,11 +9,11 @@ router.get('/admin/categories/new', (req, res) => {
 
 router.post('/categories/save', (req, res) => {
 	let title = req.body.title;
-	if(title != undefined && title != '') {
+	if (title != undefined && title != '') {
 		Category.create({
 			title,
 			slug: slugify(title)
-		}).then(()=>{
+		}).then(() => {
 			res.redirect('/admin/categories');
 		});
 	} else {
@@ -23,19 +23,19 @@ router.post('/categories/save', (req, res) => {
 
 router.get('/admin/categories', (req, res) => {
 	let categories = Category.findAll().then(categories => {
-		res.render('admin/categories/index', {categories})
+		res.render('admin/categories/index', { categories })
 	});
 });
 
 router.post('/categories/delete', (req, res) => {
 	let id = req.body.id;
-	if(id != undefined) {
-		if(!isNaN(id)) {
+	if (id != undefined) {
+		if (!isNaN(id)) {
 			Category.destroy({
 				where: {
 					id: id
 				}
-			}).then(()=>{
+			}).then(() => {
 				res.redirect('/admin/categories');
 			})
 		} else {
@@ -48,18 +48,32 @@ router.post('/categories/delete', (req, res) => {
 
 router.get('/admin/categories/edit/:id', (req, res) => {
 	let id = req.params.id;
-	if(isNaN(id)) {
+	if (isNaN(id)) {
 		res.redirect('/admin/categories');
 	}
 	Category.findByPk(id).then(category => {
-		if(category != undefined) {
-			res.render('admin/categories/edit', {category});
+		if (category != undefined) {
+			res.render('admin/categories/edit', { category });
 		} else {
 			res.redirect('/admin/categories');
 		}
 	}).catch(error => {
 		res.redirect('/admin/categories');
 	})
+});
+
+router.post('/categories/update', (req, res) => {
+	let id = req.body.id;
+	let title = req.body.title;
+	let slug = slugify(title);
+
+	Category.update({
+		title, slug
+	}, {
+		where: { id }
+	}).then(()=>{
+		res.redirect('/admin/categories');
+	});
 });
 
 module.exports = router;
