f063e2ca6fce5af6293f90aff2da80c31a318338
Julio Cesar <juliocsrmf@gmail.com>
2021-04-12 13:32:08 -0300

    Formulario de edição de categoria

--- app/categories/CategoriesController.js
+++ app/categories/CategoriesController.js
@@ -44,6 +44,22 @@ router.post('/categories/delete', (req, res) => {
 	} else {
 		res.redirect('/admin/categories');
 	}
-});	
+});
+
+router.get('/admin/categories/edit/:id', (req, res) => {
+	let id = req.params.id;
+	if(isNaN(id)) {
+		res.redirect('/admin/categories');
+	}
+	Category.findByPk(id).then(category => {
+		if(category != undefined) {
+			res.render('admin/categories/edit', {category});
+		} else {
+			res.redirect('/admin/categories');
+		}
+	}).catch(error => {
+		res.redirect('/admin/categories');
+	})
+});
 
 module.exports = router;
