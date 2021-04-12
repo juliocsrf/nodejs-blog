88fa43e8f029e1d6f605b20979b7541ce6755068
Julio Cesar <juliocsrmf@gmail.com>
2021-04-12 13:07:07 -0300

    Listagem de categorias

--- app/categories/CategoriesController.js
+++ app/categories/CategoriesController.js
@@ -22,7 +22,9 @@ router.post('/categories/save', (req, res) => {
 });
 
 router.get('/admin/categories', (req, res) => {
-	res.render('admin/categories/index')
+	let categories = Category.findAll().then(categories => {
+		res.render('admin/categories/index', {categories})
+	});
 });
 
 module.exports = router;
