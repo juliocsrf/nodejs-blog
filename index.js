7c2eb9279d140cb17329ac17e95c9e8684d75b1c
Julio Cesar <juliocsrmf@gmail.com>
2021-04-12 23:33:16 -0300

    HomePage

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ app.use('/', articlesController);
 
 //Routes
 app.get('/', (req, res) => {
-	res.render('index');
+	Article.findAll().then((articles)=>{
+		res.render('index', {articles})
+	});
 });
 
 app.listen(8080, () => {
